92092a299cfac40a2298450801fe3aa56daca8c4
QLiu84 <qliu84@its.jnj.com>
2018-09-13 16:47:53 +0800

    bug fix

--- jt-parent/jt-manage/jt-manager-service/src/main/java/com/jt/manage/service/ItemServiceImpl.java
+++ jt-parent/jt-manage/jt-manager-service/src/main/java/com/jt/manage/service/ItemServiceImpl.java
@@ -10,17 +10,16 @@ import com.jt.manage.mapper.ItemMapper;
 import com.jt.manage.pojo.Item;
 @Service
 public class ItemServiceImpl implements ItemService {
-	
+
 	@Autowired
 	private ItemMapper itemMapper;
-	
-	@Override
+
 	public List<Item> findItemList() {
-		
+
 		return itemMapper.findItemList();
 	}
 
-	
+
 	/**
 	 * 分页的业务逻辑
 	 * 0-20   SELECT * FROM tb_item ORDER BY updated DESC LIMIT 0,20   第1页
@@ -29,29 +28,27 @@ public class ItemServiceImpl implements ItemService {
 	 * 40-60  SELECT * FROM tb_item ORDER BY updated DESC LIMIT (page-1)* rows,20 第n页
 	 * easyUI的页面格式要求   {"total":2000,"rows":[{},{},{}]}
 	 */
-	@Override
 	public EasyUIResult findItemPageList(int page, int rows) {
-		
+
 		/**
 		 * JPA操作  item(id =1,name=2)
 		 * 会自动根据对象中不为null的数据  拼接sql语句
 		 * select * from tb_item where id = 1 and name = 2;
 		 */
-		
+
 		int total = itemMapper.selectCount(null); //使用通用mapper的查询操作
-		
+
 		//表示分页后的数据
 		int start = (page - 1) * rows;
 		List<Item> itemList = itemMapper.findItemPageList(start,rows);
-		
-		
+
+
 		return new EasyUIResult(total, itemList);
 	}
 
 
-	@Override
 	public String findItemCatNameByItemId(Long itemId) {
-		
+
 		return itemMapper.findItemCatNameByItemId(itemId);
 	}
 
